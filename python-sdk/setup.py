b2de94bd9d73472fe7f6f64f7c8df3daf86ee526
Josh Sephton <josh.sephton@gmail.com>
2025-06-26 15:21:39 +0100

    chore: provide a project description for python sdk

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="traintrack-mlops",
     version="0.1.0",
+    description="Traintrack is a modular MLOps platform designed to manage and monitor the full lifecycle of machine learning workflows — from dataset tracking to deployment — with a robust Go-based backend and an easy-to-use Python SDK.",
     packages=find_packages(),
     install_requires=[
         "requests",
