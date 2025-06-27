5654d01cd314dade226eb458bb5991f3a33e0a6e
Josh Sephton <josh.sephton@gmail.com>
2025-06-27 08:47:26 +0100

    chore: remove local identifier from version

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -7,7 +7,7 @@ long_description = readme.read_text() if readme.exists() else ""
 
 setup(
     name="traintrack-mlops",
-    version="0.0.1",
+    version="0.0.1a0",
     description="Traintrack is a modular MLOps platform designed to manage and monitor the full lifecycle of machine learning workflows — from dataset tracking to deployment — with a robust Go-based backend and an easy-to-use Python SDK.",
     long_description=long_description,
     long_description_content_type="text/markdown",
